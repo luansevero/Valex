761769b88fb481758d6e049d8dfa89f3a8a5c953
Luan Severo <luansevero@icloud.com>
2022-09-02 13:26:05 -0300

    Init Commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routers/index";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(router);
+
+const PORT: number = Number(process.env.PORT) || 5000
+
+app.listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
