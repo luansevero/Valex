8bbd9be73a5435a4c2915857e01529cacb973882
Luan Severo <luansevero@icloud.com>
2022-09-03 08:44:57 -0300

    [Feat] CreateCard: employee new card validation

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,12 +1,15 @@
 import { TransactionTypes } from "../repositories/cardRepository.js";
 import { findByApiKey } from "../repositories/companyRepository.js";
 import { findById } from "../repositories/employeeRepository.js";
+import * as cardRepository from "../repositories/cardRepository.js";
 
 //Services
     //#Create Card Service
     export async function createCard(apiKey:string, employeeId:number, type:TransactionTypes){
         await findByApiKey(apiKey);
         const employee = await validateEmployee(employeeId);
+        await validateEmployeeNewCardType(type, employeeId);
+        
     };
 
 //Validations
@@ -18,4 +21,8 @@ async function validateEmployee(employeeId:number){
     const employeer = await findById(employeeId);
     //Verificar se ele pertence a empresa!
     if(!employeer) throw Error("Only registered employeers can have cards!");
+};
+async function validateEmployeeNewCardType(cardType:any, employeeId:number){
+    const employeerCardboard = await cardRepository.findByTypeAndEmployeeId(cardType, employeeId);
+    if(employeerCardboard) throw Error("Already have that type of card!");
 };
