ba3fe3a71343a430a81d6cc0315f1522180163d5
Luan Severo <luansevero@icloud.com>
2022-09-03 08:40:32 -0300

    [Feat] CreateCard: company validation

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
+import { TransactionTypes } from "../repositories/cardRepository.js";
 import * as cardService from "../services/cardService.js"
 
 export async function newCard(req:Request, res:Response){
     const { apiKey } : { apiKey: string } = res.locals.apiKey
-    const { userId, type } : { userId:number, type:string } = req.body;
+    const { userId, type } : { userId:number, type:TransactionTypes } = req.body;
     if(!userId || !type) return res.sendStatus(422);
 
     await cardService.createCard(apiKey, userId, type);
--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,3 +1,14 @@
-export async function createCard(){
+import { TransactionTypes } from "../repositories/cardRepository.js";
+import { findByApiKey } from "../repositories/companyRepository.js";
 
+//Services
+    //#Create Card Service
+    export async function createCard(apiKey:string, userId:number, type:TransactionTypes){
+        await findByApiKey(apiKey);
+    };
+
+//Validations
+async function validateCompany(apiKey: string){
+    const company = await findByApiKey(apiKey);
+    if(!company) throw Error("That key don't belong to any company!");
 };
